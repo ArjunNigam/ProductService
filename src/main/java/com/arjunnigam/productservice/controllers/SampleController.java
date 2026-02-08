bde371791a8828d2864f3e07391bb255418f6972
Arjun Nigam <arjunnigam.1713@gmail.com>
2026-02-08 21:26:04 +0530

    Sample APIs in ProductService.

---
+++ src/main/java/com/arjunnigam/productservice/controllers/SampleController.java
@@ -0,0 +1,45 @@
+package com.arjunnigam.productservice.controllers;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController // This annotation indicates that this class is a Spring MVC Controller
+// Rest Controller means it is going to host REST HTTP   APIs (Web APIs)
+@RequestMapping("/sample") //endpoint = /sample
+public class SampleController {
+/*
+amazon.in/orders => OrderController
+If any request comes to /orders pass it to OrderController
+
+amazon.in/payments => PaymentController
+If any request comes to /payments , pass it to PaymentController
+
+ */
+
+    //localhost:8080/sample/hello -> SampleController.sayHello()
+    @GetMapping("/hello/{name}/{times}")
+    public String sayHello(@PathVariable("name") String name,
+                           @PathVariable("times") int x)
+    {
+        String s = "";
+        for (int i = 0; i <= x; i++)
+        {
+            s += "Hello " + name + "<br>"; // <br> is new line in HTML
+        }
+        return s;
+    }
+
+    //localhost:8080/sample/hi -> SampleController.sayHi()
+    @GetMapping("/hi")
+    public String sayHi()
+    {
+        return "Hi";
+    }
+
+
+
+
+
+
+}
